80c69822ad6e005a0b5a7fb65659efd694d351df
Alexandr <master.tatu.91@bk.ru>
2020-10-31 09:58:43 +0400

    Update AceEngine

---
+++ examples/polygon.cpp
@@ -0,0 +1,30 @@
+#include <AceEngine/AceEngine.hpp>
+
+ACE::window window;
+ACE::polygon polygon;
+SDL_Event event;
+
+int main()
+{
+    window.create("polygon", ACE_WINDOW_POS_CENTERED, ACE::vector2<int>(800, 800));
+
+    polygon.set_point_count(4);
+    polygon.set_point_position(0, ACE::vector2<float>(0, 0));
+    polygon.set_point_position(1, ACE::vector2<float>(100, 0));
+    polygon.set_point_position(2, ACE::vector2<float>(100, 100));
+    polygon.set_point_position(3, ACE::vector2<float>(0, 100));
+    polygon.set_fill_color({255, 0, 0, 255});
+
+    while (window.is_open())
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (window.quit(event))
+                window.close();
+        }
+
+        window.clear();
+        polygon.show_filled();
+        window.display();
+    }
+}
