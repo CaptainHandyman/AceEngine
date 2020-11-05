914eaa48f294f8edc7051901f0c502e40cbde6a0
Alexandr <master.tatu.91@bk.ru>
2020-11-06 00:19:19 +0400

    Update AceEngine

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
-sudo apt-get update
-sudo apt-get install libsdl2-dev \
-    libsdl2-image-dev \
-    libsdl2-ttf-dev \
-    mesa-common-dev g++
+shared_install() {
+    sudo apt-get update
+    sudo apt-get install libsdl2-dev \
+        libsdl2-image-dev \
+        libsdl2-ttf-dev \
+        mesa-common-dev g++
 
-if [ -d /usr/include/AceEngine ] ||
-    [ -d /usr/lib/x86_64-linux-gnu/libAceEngine.so ]; then
-    sudo make -f MakeFile uninstall
-fi
-make -f MakeFile
-sudo make -f MakeFile install
+    if [ -d /usr/include/AceEngine ] ||
+        [ -d /usr/lib/x86_64-linux-gnu/libAceEngine.so ]; then
+        sudo make -f MakeFile uninstall
+    fi
+    make -f MakeFile
+    sudo make -f MakeFile install
+
+}
+
+while getopts 'dlu' option; do
+    case $option in
+    d) shared_install ;;
+    l) make -f MakeFile ;;
+    u) make -f MakeFile uninstall ;;
+    esac
+done
