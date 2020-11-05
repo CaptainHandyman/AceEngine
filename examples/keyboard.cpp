01b1b59fc638de67439dcd6c543e2894d7912241
Alexandr <master.tatu.91@bk.ru>
2020-11-05 12:12:11 +0400

    Update AceEngine

---
+++ examples/keyboard.cpp
@@ -0,0 +1,26 @@
+#include <AceEngine/AceEngine.hpp>
+#include <iostream>
+using namespace std;
+
+ACE::window window;
+SDL_Event event;
+
+int main()
+{
+    window.create("window", ACE_WINDOW_POS_CENTERED, ACE::vector2<int>(800, 800));
+
+    while (window.is_open())
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (window.quit(event))
+                window.close();
+        }
+
+        if(ACE::keyboard::key_pressed(SDL_SCANCODE_D))
+            cout << "Key 'D' was pressed!" << endl;
+
+        window.clear();
+        window.display();
+    }
+}
