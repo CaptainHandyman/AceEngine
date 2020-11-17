d135db16a7225a2e5f17e42a188efe9d6ee5d780
Alexandr <master.tatu.91@bk.ru>
2020-11-17 19:42:59 +0400

    Update AceEngine

--- configure.sh
+++ configure.sh
@@ -1,3 +1,4 @@
 sudo apt-get install libsdl2-dev \
                      libsdl2-image-dev \
-                     libsdl2-ttf-dev
+                     libsdl2-ttf-dev \
+                     mesa-common-dev
