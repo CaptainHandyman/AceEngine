f30bafcf14ef31de9353cb2a7cdc8d2fdb24cfb4
Alexandr <master.tatu.91@bk.ru>
2020-11-26 16:11:59 +0400

    Update header_files/AceEngine.hpp

--- header_files/structures.hpp
+++ header_files/structures.hpp
@@ -3,7 +3,7 @@
  * @email alexandralibekov@yahoo.com
  * @create date 2020-10-28 14:48:19
  * @modify date 2020-11-04 01:55:34
- * @version 0.2
+ * @version 0.21
  * @desc This program is free software.
  *       You can redistribute it and/or modify.
  */
